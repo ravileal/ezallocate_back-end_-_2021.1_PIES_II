42dca50569801b6f9896949cd280e0386ddfb82d
Ravi Leal <pauloravileal@outlook.com>
2021-07-22 07:11:44 -0300

    chore: configuracao de style guides

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+import express from 'express';
+
+const app: express.Application = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(3000, () => {
+  console.log('App is listening on port 3000! http://localhost:3000');
+});
