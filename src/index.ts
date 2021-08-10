437ffac49df07a6cbf9f00efebda8de29e6bbffe
Ravi Leal <pauloravileal@outlook.com>
2021-08-10 18:52:50 -0300

    chore: remocao de comentarios

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import moment from 'moment';
 const cors = require('cors');
 
 const boot = require('./app/boot');
@@ -9,10 +8,5 @@ app.use(cors());
 app.use(express.json());
 boot(app);
 app.listen(3000, () => {
-
-  const date = "2021-08-31";
-  const enWeek = moment(date).format("dddd");
-  console.log(enWeek);
-
   console.log('App is listening on port 3000! http://localhost:3000');
 });
